47981bf6a3cf2d24499aa8d6c0c159aa8f184674
unknown <idanyildizi@hotmail.com>
2020-05-18 23:05:33 +0200

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -3,5 +3,6 @@ package tests;
 public class Spring {
     public static void main(String[] args) {
         System.out.println("new one");
+        System.out.println("winter is here already");
     }
 }
